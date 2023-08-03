72d1c529b17ef88f7676ad4d464941b6679f4070
Terry-Rossi <terryrossi1@gmail.com>
2023-08-03 13:32:11 -0700

    First commit with e functions running

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,18 +1,121 @@
 'use strict';
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const { google } = require('googleapis');
+const calendar = google.calendar('v3');
+const SCOPES = ['https://www.googleapis.com/auth/calendar.events.public.readonly'];
+const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
+const redirect_uris = ['https://terryrossi.github.io/meet/'];
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, redirect_uris[0]);
+
+module.exports.getAuthURL = async () => {
+	/**
+	 *
+	 * Scopes array is passed to the `scope` option.
+	 *
+	 */
+	const authUrl = oAuth2Client.generateAuthUrl({
+		access_type: 'offline',
+		scope: SCOPES,
+	});
+
+	return {
+		statusCode: 200,
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Credentials': true,
+		},
+		body: JSON.stringify({
+			authUrl,
+		}),
+	};
+};
+
+module.exports.getAccessToken = async (event) => {
+	// Decode authorization code extracted from the URL query
+	const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+	return new Promise((resolve, reject) => {
+		/**
+		 *  Exchange authorization code for access token with a “callback” after the exchange,
+		 *  The callback in this case is an arrow function with the results as parameters: “error” and “response”
+		 */
+
+		oAuth2Client.getToken(code, (error, response) => {
+			if (error) {
+				return reject(error);
+			}
+			return resolve(response);
+		});
+	})
+		.then((results) => {
+			// Respond with OAuth token
+			return {
+				statusCode: 200,
+				headers: {
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Credentials': true,
+				},
+				body: JSON.stringify(results),
+			};
+		})
+		.catch((error) => {
+			// Handle error
+			return {
+				statusCode: 500,
+				body: JSON.stringify(error),
+			};
+		});
+};
+
+// GetCalendar Function
+
+module.exports.getCalendarEvents = async (event) => {
+	// Declare an access_token variable and get the token
+	const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+
+	//Set the access token as credentials in oAuth2Client
+	oAuth2Client.setCredentials({ access_token });
+
+	return new Promise((resolve, reject) => {
+		/**
+		 *  This calendar method will get a list of events from the “fullstackwebdev”
+		 *  Google calendar using oAuth2Client for authentication
+		 */
+
+		calendar.events.list(
+			{
+				calendarId: CALENDAR_ID,
+				auth: oAuth2Client,
+				timeMin: new Date().toISOString(),
+				singleEvents: true,
+				orderBy: 'startTime',
+			},
+			(error, response) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(response);
+				}
+			}
+		);
+	})
+		.then((results) => {
+			// Returns Calendar Events
+			return {
+				statusCode: 200,
+				headers: {
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Credentials': true,
+				},
+				body: JSON.stringify({ events: results.data.items }),
+			};
+		})
+		.catch((error) => {
+			// Handle error
+			return {
+				statusCode: 500,
+				body: JSON.stringify(error),
+			};
+		});
 };
