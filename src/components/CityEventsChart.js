e6b5cf1565e2bdd6b1d0b36da3f56fd5925df8da
Terry-Rossi <terryrossi1@gmail.com>
2023-08-15 14:03:33 -0700

    Finalized ScatterPlot Graph

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -21,7 +21,8 @@ const CityEventsChart = ({ allLocations, events }) => {
 	const getData = () => {
 		const data = allLocations.map((location) => {
 			const count = events.filter((event) => event.location === location).length;
-			const city = location.split(', ')[0];
+			const city = location.split(/, | - /)[0];
+			// console.log(count, city);
 			return { count, city };
 		});
 		return data;
@@ -34,19 +35,23 @@ const CityEventsChart = ({ allLocations, events }) => {
 				margin={{
 					top: 20,
 					right: 20,
-					bottom: 20,
-					left: 20,
+					bottom: 60,
+					left: -30,
 				}}>
 				<CartesianGrid />
 				<XAxis
 					type='category'
 					dataKey='city'
 					name='City'
+					angle={60}
+					interval={0}
+					tick={{ dx: 20, dy: 40, fontSize: 14 }}
 				/>
 				<YAxis
 					type='number'
 					dataKey='count'
 					name='Number of events'
+					allowDecimals={false}
 				/>
 				<Tooltip cursor={{ strokeDasharray: '3 3' }} />
 				<Scatter
